d4d6759789448fa63a87f17b1ae9df14397962f8
gvanderclay <gvanderclay@yahoo.com>
2017-02-03 13:13:51 -0500

    fix some errors

--- lab04/multiServer.c
+++ lab04/multiServer.c
@@ -3,34 +3,34 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include<time.h>
+#include <time.h>
 
 #define true 1
 #define false 0
-void * accessFile(void *arg);
-
+void *accessFile(void *arg);
 
 int main() {
   char buffer[256];
-  while(true) {
+  srand(time(NULL));
+  while (true) {
     printf("Enter a filename: ");
-    if(fgets(buffer, sizeof(buffer), stdin)) {
+    if (fgets(buffer, sizeof(buffer), stdin)) {
       // remove newline from end of string
       size_t ln = strlen(buffer) - 1;
-      if(buffer[ln] == '\n') {
+      if (buffer[ln] == '\n') {
         buffer[ln] = '\0';
       }
       printf("%s\n", buffer);
-    }
-    else {
+    } else {
       printf("fgets error\n");
       exit(1);
     }
   }
+}
 
-void* accessFile(void *arg){
-	int delay = rand(time(NULL))%4;
-	if(delay < 3){
-		sleep(1);
-	}
+void *accessFile(void *arg) {
+  int delay = rand() % 4;
+  if (delay < 3) {
+    sleep(1);
+  }
 }
