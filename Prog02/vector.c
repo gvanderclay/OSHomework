5b581d39598a2c706a394eabd44e498f9a7e87fb
gvanderclay <gvanderclay@yahoo.com>
2017-02-13 17:23:07 -0500

    fix some things

--- Prog02/vector.c
+++ Prog02/vector.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
@@ -8,6 +10,8 @@
 
 #define READ 0
 #define WRITE 1
+
+#define MAX_BUFF_SIZE 65
 
 char * getInput();
 
@@ -17,9 +21,9 @@ void complementer(int);
 
 void incrementer(int, int);
 
-void flip(int *);
+void flip(char *);
 
-char * increment(char *);
+void increment(char *);
 
 void printLog(const char *, ...);
 
@@ -51,6 +55,7 @@ int main() {
     printLog("Spawning incrementer process....\n");
     if((incrementerPID = fork()) == -1) {
       printLog("Error forking incrementer");
+      exit(1);
     }
   }
 
@@ -58,10 +63,12 @@ int main() {
     printLog("Spawning adder process....\n");
     if((adderPID = fork()) == -1) {
       printLog("Error forking adder");
+      exit(1);
     }
   }
 
   if(complementerPID == 0) {
+    printLog("Complementer spawned\n");
     // close the unused pipes for this process
     printLog("Closing pipes for complementer\n");
     close(ItoA[READ]);
@@ -73,7 +80,8 @@ int main() {
     printLog("Running complementer function\n");
     complementer(CtoI[WRITE]);
   }
-  if(incrementerPID == 0) {
+  if(incrementerPID == 0 && complementerPID != 0) {
+    printLog("Incrementer spawned\n");
     // close unused pipes
     printLog("Closing pipes for incrementer\n");
     close(CtoI[WRITE]);
@@ -83,23 +91,26 @@ int main() {
     incrementer(CtoI[READ], ItoA[WRITE]);
   }
   if(adderPID == 0) {
+    printLog("Adder spawned\n");
     printLog("Closing pipes for adder\n");
-    close(CtoI[WRITE]);
-    close(CtoI[READ]);
-    close(ItoA[WRITE]);
+    /* close(CtoI[WRITE]); */
+    /* close(CtoI[READ]); */
+    /* close(ItoA[WRITE]); */
     // TODO implement adder function
   }
 
   if(complementerPID != 0 && incrementerPID != 0 && adderPID != 0) {
+    printLog("Closing pipes in main process\n");
     close(CtoI[READ]);
     close(CtoI[WRITE]);
     close(ItoA[READ]);
     close(ItoA[WRITE]);
+    int status;
     // wait for all child processes to end
-    while(waitpid(-1, NULL, 0)) {
-      if(errno == ECHILD) {
-        break;
-      }
+    pid_t wpid;
+    printLog("Waiting for child processes to end\n");
+    while((wpid = wait(&status)) > 0) {
+      printf("PID %d: Exit status was %d\n", wpid, status);
     }
   }
 }
@@ -112,49 +123,61 @@ void complementer(int pipe) {
   FILE * input = readFile(inputB);
 
   //TODO pause and catch for Ctrl-C
-  int ch;
-  while((ch = fgetc(input)) != EOF) {
-    flip(&ch);
-    char buff = ch;
-    if(ch == '\n') {
-      printLog("Complementer sending: \\n\n");
+  char buff[MAX_BUFF_SIZE];
+  while(fgets(buff, MAX_BUFF_SIZE, input)) {
+    // remove newline from end of string
+    buff[strcspn(buff, "\n")] = '\0';
+
+    // flip each digit in the number
+    for(int i = 0; i < strlen(buff) - 1; i++) {
+      flip(&buff[i]);
     }
-    else {
-      printLog("Complementer sending: %c\n", buff);
-    }
-    write(pipe, &buff, 1);
+    printLog("Complementer sending: %s\n", buff);
+    write(pipe, buff, MAX_BUFF_SIZE);
   }
 
   fclose(input);
+  close(pipe);
 
   printLog("Complementer function finished\n");
-  exit(0);
+  /* exit(0); */
 }
 
 /**
  * Increment the binary numbers received from the pipe,
  * then streams them to the outPipe
- * TODO actually increment each binary number
  **/
 void incrementer(int inPipe, int outPipe) {
-  char ch;
+  char buff[MAX_BUFF_SIZE];
+  while(read(inPipe, buff, MAX_BUFF_SIZE)) {
+    printLog("Incrementer received %s\n", buff);
+    increment(buff);
+    printLog("Incrementer sending %s\n", buff);
+    write(outPipe, buff, MAX_BUFF_SIZE);
+    sleep(1);
+  }
+  printLog("Incrementer function finished\n");
+}
 
-  while(read(inPipe, &ch, 1) == 1) {
-    if(ch == '\n') {
-      printLog("Incrementer receiving: \\n\n");
+void increment(char * num) {
+  printLog("Incrementing\n");
+  int carry = 1;
+  for(int i = strlen(num) - 2; i >= 0 && carry == 1; i--) {
+    if(num[i] == '1') {
+      num[i] = '0';
     }
     else {
-    printLog("Complementer receiving: %c\n", ch);
+      num[i] = '1';
+      carry = 0;
     }
   }
-
-  printLog("Incrementer function finished\n");
+  printLog("Done incrementing\n");
 }
 
 /**
  * Flips the binary character
  */
-void flip(int * ch) {
+void flip(char * ch) {
   if(*ch == '\n') {
     return;
   }
