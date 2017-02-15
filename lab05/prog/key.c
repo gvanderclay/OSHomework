f4d735a32d803d987bd63cfab2185e128cfcb2f2
gvanderclay <gvanderclay@yahoo.com>
2017-02-15 13:57:31 -0500

    remove stuff

--- lab05/prog/key.c
+++
@@ -1,7 +0,0 @@
-#include "key.h"
-#include <sys/types.h>
-#include <sys/ipc.h>
-
-void generate_key(key_t * key) {
-  *key = ftok("makefile", 'x');
-}
--- lab05/prog/key.h
+++
@@ -1,8 +0,0 @@
-// header file to generate key
-#ifndef _key_h
-#define _key_h
-#include <sys/types.h>
-
-void generate_key(key_t *);
-
-#endif
